5c06e90a48ed25050fdfd31737514411ec95b968
Zade Viggers <zade@viggers.net>
2023-08-07 14:24:40 +1200

    Add stars

---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OntriigerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
