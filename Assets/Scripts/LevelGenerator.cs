969e7b77acd43252835d3e80d7aa1bb225582a02
Zade Viggers <zade@viggers.net>
2023-08-22 13:48:23 +1200

    Remove spawned objects

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -10,6 +10,7 @@ public class LevelGenerator : MonoBehaviour
     public int numberOfSpikes;
     public int numberOfCollectables;
     public float levelWidth;
+    public float yRandomnessMultiplier;
  
     // Prefabs
     public GripController gripPrefab;
@@ -18,10 +19,14 @@ public class LevelGenerator : MonoBehaviour
 
     // Internal state
     float lastSegmentY = 0f;
+    List<GameObject> spawnedObjects= new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
+        // Spawn a grip on the player so they don't fall
+        spawnedObjects.Add(Instantiate(gripPrefab, GameManager.theManager.player.transform.position, GameManager.theManager.player.transform.rotation).gameObject);
+
         if (segmentHeight <= 0)
         {
             Debug.LogError("Provide a value greater than 0 for segmentHeight!");
@@ -30,23 +35,47 @@ public class LevelGenerator : MonoBehaviour
         }
     }
 
+    float RandomX()
+    {
+        return Random.Range(-(levelWidth / 2), levelWidth / 2);
+    }
+
+    float RandomYOffset()
+    {
+        return Random.Range(-1, 1) * yRandomnessMultiplier;
+    }
+
     void GenerateNextSegment()
     {
         // Spawn grips
         for (int i = 0; i < numberOfGrips; i++)
         {
-            float xPos = Random.Range(-(levelWidth / 2), levelWidth / 2);
-            float yPos = lastSegmentY + (i * (segmentHeight / numberOfGrips));
-            Instantiate(gripPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity);
+            float yPos = lastSegmentY + (i * (segmentHeight / numberOfGrips)) + RandomYOffset();
+            spawnedObjects.Add(Instantiate(gripPrefab, new Vector3(RandomX(), yPos, 0), Quaternion.identity).gameObject);
         }
         lastSegmentY += segmentHeight;
     }
 
     public void HandlePlayerMoved() {
-        float currentPlayerY = GameManager.theManager.player.transform.position.y;
-        if (currentPlayerY + segmentHeight >= lastSegmentY)
+        if (GameManager.theManager.player.transform.position.y + segmentHeight >= lastSegmentY)
         {
             GenerateNextSegment();
+            RemoveOldSpawnedObjects();
         }
+    }
+
+    public void RemoveOldSpawnedObjects()
+    {
+        List<GameObject> toRemove = new List<GameObject>();
+        foreach (GameObject obj in spawnedObjects)
+        {
+            if (obj.transform.position.y < lastSegmentY - (3*segmentHeight))
+            {
+                Destroy(obj);
+                toRemove.Add(obj);
+            }
+        }
+        // Can't remove from a list while looping over it
+        toRemove.ForEach((el)=>spawnedObjects.Remove(el));
     }
 }
